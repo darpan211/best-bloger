b20f944f5741f543e5acb7ac64f2bab62ca090c8
krishnadobariya <krishnadobariya3488@gmail.com>
2023-03-27 10:23:13 +0530

    CHANGES: next-seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,9 +2,10 @@ export default {
     title: "Blogs",
     description: "Welcome to Blogs, your go-to source for Topics. Our team of expert writers share informative and engaging articles on AI, games, Machine Learning, providing you with the latest news, tips, and insights to help you Achieve Specific Goals Join our community today and stay informed on all things.",
     openGraph: {
-        type: 'website',
+        title: "Blogs",
+        description: "Welcome to Blogs, your go-to source for Topics. Our team of expert writers share informative and engaging articles on AI, games, Machine Learning, providing you with the latest news, tips, and insights to help you Achieve Specific Goals Join our community today and stay informed on all things.",
         locale: 'en_IE',
-        url: "",
+        url: "https://beastbloger.com/",
         site_name: 'Blogs',
         images: [
             {
