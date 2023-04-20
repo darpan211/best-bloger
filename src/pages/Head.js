ceebd1752ad9b5c646518c8e39599cb2b747f080
dkproduction18 <61166032+dkproduction18@users.noreply.github.com>
2023-04-20 18:49:00 +0530

    Update Head.js

--- src/pages/Head.js
+++ src/pages/Head.js
@@ -9,8 +9,9 @@ function Heads() {
           rel="shortcut icon"
           href="https://firebasestorage.googleapis.com/v0/b/sharecode-84ab8.appspot.com/o/oct-logo.png?alt=media&token=c4c94729-f41d-45a2-8f6b-8723f9616903"
         />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4843229773318635"
-          crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4843229773318635"
+     crossorigin="anonymous"></script>
+        
         <script
           async
           src={data?.script}
